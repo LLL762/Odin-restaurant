1be92200478944b82f447cae44043c97200c2023
LLL762 <lamirallaurent@gmail.com>
2022-09-23 10:27:44 +0200

    menu improvement+ home header

--- src/menu-bar/menu-bar.js
+++ src/menu-bar/menu-bar.js
@@ -3,21 +3,57 @@ import "./menu-bar.css";
 import { Navigation } from "../navigation/navigation";
 
 const MenuBarComponent = () => {
-  const init = () => {
-    show();
+  const selectedStyleClass = "menu-item-selected";
 
-    const homeBtn = document.querySelector("header #btn-home");
-    const menuBtn = document.querySelector("header #btn-menu");
-    const contactBtn = document.querySelector("header #btn-contact");
-
-    homeBtn.addEventListener("click", () => Navigation.goToHome());
-    menuBtn.addEventListener("click", () => Navigation.goToMenu());
-    contactBtn.addEventListener("click", () => Navigation.goToContact());
-  };
+  let currPageBtn;
 
   const show = () => {
     const heardElem = document.querySelector("header");
     heardElem.innerHTML = template;
+  };
+
+  const init = () => {
+    show();
+    const homeBtn = document.querySelector("header #btn-home");
+    const menuBtn = document.querySelector("header #btn-menu");
+    const contactBtn = document.querySelector("header #btn-contact");
+
+    setCurrPageBtn(homeBtn);
+
+    homeBtn.addEventListener("click", (event) => homeBtnOnClick(event));
+    menuBtn.addEventListener("click", (event) => menuBtnOnClick(event));
+    contactBtn.addEventListener("click", (event) => contactBtnOnClick(event));
+  };
+
+  const homeBtnOnClick = (event) => {
+    Navigation.goToHome();
+    setCurrPageBtn(event.currentTarget);
+  };
+
+  const menuBtnOnClick = (event) => {
+    Navigation.goToMenu();
+    setCurrPageBtn(event.currentTarget);
+  };
+
+  const contactBtnOnClick = (event) => {
+    Navigation.goToContact();
+    setCurrPageBtn(event.currentTarget);
+  };
+
+  const setCurrPageBtn = (newvalue) => {
+    if (currPageBtn) {
+      clearSelectedStyle();
+    }
+    currPageBtn = newvalue;
+    applySelectedStyle();
+  };
+
+  const applySelectedStyle = () => {
+    currPageBtn.classList.add(selectedStyleClass);
+  };
+
+  const clearSelectedStyle = () => {
+    currPageBtn.classList.remove(selectedStyleClass);
   };
 
   return { init };
